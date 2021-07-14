842b1c87f225e2ce982a53baec1bd8ea92f7bad1
ram-krishna-1 <86171231+ram-krishna-1@users.noreply.github.com>
2021-07-14 15:34:24 -0500

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,34 +1,94 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer
 from requests.auth import HTTPBasicAuth
 
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-
+def get_request(url, api_key=None, **kwargs):
+    print(kwargs)
+    print("From {} ".format(url))
+    try:
+        if api_key:
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs, auth = HTTPBasicAuth("apikey", api_key))
+        # Call get method of requests library with URL and parameters
+        else:
+            request.get(url, params = kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
-
+def post_request(url, json_payload, **kwargs):
+    response = requests.post(url, headers={'Content-Type': 'application/json'},
+                                json=json_payload, params=kwargs))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["rows"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
 
+    return results
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
-
-
+def get_dealer_reviews_from_cf(url, dealerId):
+    results = []
+    json_request = get_request(url, dealerId = dealerId)
+    if json_result:
+        reviews = json_result["entries"]
+        for review_s in reviews:
+            review_obj = DealerReview(car_make=review_s['car_make'], car_model=review_s['car_model'], 
+            car_year=review_s['car_year'], id=review_s['id'], dealership=review_s['dealership'], 
+            name=review_s['name'], purchase=review_s['purchase'], purchase_date=review_s['purchase_date'], 
+            review=review_s['review'], sentiment="")
+            review_obj[sentiment] = analyze_review_sentiments(review_obj.review)
+            results.append(review_obj)
+    return results
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
+def analyze_review_sentiments(text):
+    params = dict()
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/80a8ea1d-d4bc-4b92-a4f6-9b6069df85ee"
+    api_key = "5F9HfLPwDhK6NWT3ACAJxjmT6ABwZsuN9qVznWM7QzVe"
+    params["text"] = kwargs["text"]
+    params["version"] = kwargs["version"]
+    params["features"] = kwargs["features"]
+    params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+    response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                                    auth=HTTPBasicAuth('apikey', api_key))
+    result = response["sentiment"]["document"]["label"]
+    return result
 
 
