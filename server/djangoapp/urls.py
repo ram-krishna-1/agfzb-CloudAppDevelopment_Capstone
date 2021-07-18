d5f9277cfb4394fd355ffc29f1847872ac0d675a
ram-krishna-1 <86171231+ram-krishna-1@users.noreply.github.com>
2021-07-17 20:58:10 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     # path for registration
     path(route="registration", view=views.registration_request, name = "registration"),
     # path for login
-    path(route="", view = views.login_request, name = "login"),
+    path(route="login", view = views.login_request, name = "login"),
     # path for logout
     path(route="logout", view = views.logout_request, name = "logout"),
     path(route='', view=views.get_dealerships, name='index'),
